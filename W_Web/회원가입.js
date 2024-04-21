32cd697d41689ce4a3db8ac300c08f990a7e5135
kiah7001 <163372949+kiah7001@users.noreply.github.com>
2024-04-21 22:27:51 +0900

    Add files via upload

---
+++ W_Web/회원가입.js
@@ -0,0 +1,29 @@
+document.getElementById('passwordInput').addEventListener('click', function() {
+    document.getElementById('passwordGuidelines').style.display = 'block';
+  });
+  
+  document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    let idInput = document.getElementById('idInput');
+    let passwordInput = document.getElementById('passwordInput');
+    let passwordConfirmInput = document.getElementById('passwordConfirmInput');
+  
+    if (idInput.value.trim() === '') {
+        document.getElementById('idError').style.display = 'block';
+    } else {
+        document.getElementById('idError').style.display = 'none';
+    }
+  
+    if (passwordInput.value.trim() === '') {
+        document.getElementById('passwordError').style.display = 'block';
+    } else {
+        document.getElementById('passwordError').style.display = 'none';
+    }
+  
+    if (passwordConfirmInput.value.trim() === '') {
+        document.getElementById('passwordConfirmError').style.display = 'block';
+    } else {
+        document.getElementById('passwordConfirmError').style.display = 'none';
+    }
+  });
